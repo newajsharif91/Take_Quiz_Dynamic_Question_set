da1b921fe3b933aef9ec255d06b703c3baa76455
“[newajsharif91]” <[email>
2022-10-12 11:08:30 +0600

    set layout

---
+++ src/layout/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+
+const Main = () => {
+  return (
+    <div className="main-container">
+      <Header></Header>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
