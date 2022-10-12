4ff589dd14d87ef8d55cb5f6012d79bd7ea7d11c
“[newajsharif91]” <[email>
2022-10-12 11:47:47 +0600

    add all after facing some problem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layout/Main';
+import Home from './components/Home/Home';
+import Blog from './components/Blog/Blog';
+import Quizes from './components/Quizes/Quizes';
+import Chart from './components/Chart/Chart';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element:<Main></Main>,
+      children: [
+        {
+          path: '/',
+          loader: () =>  fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <Home></Home>
+        },
+        {
+          path:'/quizes/:id',
+          loader: async ({params}) => {
+            return fetch(`https://openapi.programming-hero.com/api/quiz/${params.id}`)
+        },
+          element: <Quizes></Quizes>
+        },
+        {
+          path: '/chart',
+          loader: () =>  fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <Chart></Chart>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        }
+
+      ]
+    }
+  ])
+
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
